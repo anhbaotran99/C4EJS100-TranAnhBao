b02957bf087368f749224a8c37a22b67619e303f
anhbaotran99 <anhbao.tran.99@gmail.com>
2020-11-23 18:19:43 +0700

    Update project

--- C4EJS100-TranAnhBao/Shopaholic/js/components.js
+++ C4EJS100-TranAnhBao/Shopaholic/js/components.js
@@ -1,3 +1,4 @@
+// Đổ data vào html
 let product_items = document.getElementById("product_items");
 
 function showProducts(image, name, price, sold, brand, place)
@@ -21,14 +22,75 @@ function showProducts(image, name, price, sold, brand, place)
             </div>
             </a>
         </div>
-            
     `
     )
 }
 
+// Mặc định show tất cả sản phẩm
 product_items.innerHTML = '';
 for(let i = 0; i < products.length; i++)
 {
     showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
 }
 
+// Show tất cả sản phẩm khi click vào
+let showAll = document.getElementById('all');
+showAll.addEventListener('click', () => {
+    product_items.innerHTML = '';
+    for(let i = 0; i < products.length; i++)
+    {
+        showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
+    }
+})
+
+// Show thiết bị điện tử
+let showTBDT = document.getElementById('thietBiDienTu');
+showTBDT.addEventListener('click', () => {
+    product_items.innerHTML = '';
+    for(let i = 0; i < products.length; i++)
+    {
+        if(products[i].category == "Thiết bị điện tử")
+        {
+            showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
+        }
+    }
+})
+
+// Show sách
+let showSach = document.getElementById('sach');
+showSach.addEventListener('click', () => {
+    product_items.innerHTML = '';
+    for(let i = 0; i < products.length; i++)
+    {
+        if(products[i].category == "Sách")
+        {
+            showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
+        }
+    }
+})
+
+// Show Đồ tiêu dùng
+let showDoTieuDung = document.getElementById('doTieuDung');
+showDoTieuDung.addEventListener('click', () => {
+    product_items.innerHTML = '';
+    for(let i = 0; i < products.length; i++)
+    {
+        if(products[i].category == "Đồ tiêu dùng")
+        {
+            showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
+        }
+    }
+})
+
+// Show Thời trang
+let showThoiTrang = document.getElementById('thoiTrang');
+showThoiTrang.addEventListener('click', () => {
+    product_items.innerHTML = '';
+    for(let i = 0; i < products.length; i++)
+    {
+        if(products[i].category == "Thời trang")
+        {
+            showProducts(products[i].image, products[i].name, products[i].price, products[i].sold, products[i].brand, products[i].place);
+        }
+    }
+})
